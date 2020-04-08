b347b4e3371a91927841e7af1ad315487dced47c
Bo Flachs <b.l.flachs@students.uu.nl>
2020-04-08 22:22:33 +0200

    Geldknoppen gefixt

--- static/game.js
+++ static/game.js
@@ -5,8 +5,30 @@ var name = prompt("Please enter your name");
 socket.emit('new player', name);
 
 var localState = {};
+localState = {
+  gameStarted: false,
+  modus: 'geen',
+  ezelCount: 0,
+  ratCount: 0,
+  players: {
+    testid:{
+      isLeader: true,
+      name: "Bo",
+      money: {
+        0: 3,
+        10: 3,
+        20: 1,
+        50: 1,
+        100: 0,
+        200: 0,
+        500: 0,
+      }
+    }
+  },
+};
 
-var virtueelBedrag = {
+let virtueelBedrag = localState.players.testid.money;
+let virtueelBod = {
   0: 0,
   10: 0,
   20: 0,
@@ -21,6 +43,11 @@ var virtueelBedrag = {
 //
 // Declare and initialize DOM variables
 //
+// Declare actieknoppen
+let actieKnoppen2 = {}
+let knoppenActie = document.getElementsByClassName("actieKnop");
+
+
 // Initalize ezel en rattentellers
 let aantalEzels = document.getElementById('ezelTeller').getElementsByTagName('p')[0];
 let aantalRatten = document.getElementById('rattenTeller').getElementsByTagName('p')[0];
@@ -36,12 +63,14 @@ let naamInput = naamSubmit.previousSibling.previousSibling;
 // Initialize modus paneel
 let modusPaneel = document.getElementById("spelModus");
 
-// Initialize uitgedaagde Koehandel menu en knoppen
+// Initialize Koehandel menu en knoppen
 let uitgedaagdeKoehandelScherm = document.getElementById('uitgedaagdeKoehandelScherm');
 let tegenBodButton = document.getElementById('tegenBodButton');
 tegenBodButton.addEventListener('click', doeTegenBod, false);
 let accepteerKoehandelButton = document.getElementById('accepteerKoehandel');
 accepteerKoehandelButton.addEventListener('click', accepteerKoehandel, false);
+actieKnoppen2.Koehandel = knoppenActie[0];
+actieKnoppen2.Koehandel.addEventListener('click', updateGeldKnoppen, false);
 
 // Initialize geld plus 1 min1 knoppen
 let min1Knoppen = document.getElementsByClassName("min1Button");
@@ -53,6 +82,10 @@ for(var knop of plus1Knoppen){
   knop.addEventListener('click', verhoogBedrag, false);
 }
 
+// Initialize betaalmenu
+actieKnoppen2.Betaling = knoppenActie[3];
+actieKnoppen2.Betaling.addEventListener('click', updateGeldKnoppen, false);
+
 // Handle alle koehandel knoppen
 function accepteerKoehandel(){
   uitgedaagdeKoehandelScherm.style.display = "none";
@@ -61,35 +94,145 @@ function doeTegenBod(){
   uitgedaagdeKoehandelScherm.style.display = "none";
 }
 
+// Bereken totaal geld hulpfunctie
+function calculateTotal(money) {
+  var res = 0;
+  for (const [amount, times] of Object.entries(money)) {
+    res += times * amount;
+  }
+  return res;
+}
+
+// Disable alle geldknoppen
+function updateGeldKnoppen(){
+  for(let i = 0; i < plus1Knoppen.length; i++)
+  {
+    switch(i%7){
+      case 0:
+        if(virtueelBedrag["0"] === 0)
+          plus1Knoppen[i].disabled = true;
+        break;
+        case 1:
+        if(virtueelBedrag["10"] === 0)
+          plus1Knoppen[i].disabled = true;
+        break;
+        case 2:
+        if(virtueelBedrag["20"] === 0)
+          plus1Knoppen[i].disabled = true;
+        break;
+        case 3:
+        if(virtueelBedrag["50"] === 0)
+          plus1Knoppen[i].disabled = true;
+        break;
+        case 4:
+        if(virtueelBedrag["100"] === 0)
+          plus1Knoppen[i].disabled = true;
+        break;
+        case 5:
+        if(virtueelBedrag["200"] === 0)
+          plus1Knoppen[i].disabled = true;
+        break;
+        case 6:
+        if(virtueelBedrag["500"] === 0)
+          plus1Knoppen[i].disabled = true;
+        break;
+    }
+  }
+}
+
 // Verhoog of verlaag de bedragen in een betaling of handel
 function verlaagBedrag(e){
-  let bedragString = e.target.parentNode.previousSibling.previousSibling;
+  let bedragString = e.target.parentNode.nextSibling.nextSibling;
   let child = e.target.parentNode;
+  let plus1Knop = e.target.parentNode.nextSibling.nextSibling.nextSibling.nextSibling.children[0];
   var i = 0;
   while( (child = child.previousSibling) != null ) {
     i++;
   }
   switch(i){
     case 1:
-      console.log("Verlaag 0");
+      virtueelBod["0"]--;
+      virtueelBedrag["0"]++;
+      if(virtueelBod["0"] === 0)
+      {
+        e.target.disabled = true;
+      }
+      if(virtueelBedrag["0"] !== 0 && plus1Knop.disabled === true){
+        plus1Knop.disabled = false;
+      }
+      bedragString.innerText = virtueelBod["0"];
       break;
     case 7:
-      console.log("Verlaag 10");
+      virtueelBod["10"]--;
+      virtueelBedrag["10"]++;
+      if(virtueelBod["10"] === 0)
+      {
+        e.target.disabled = true;
+      }
+      if(virtueelBedrag["10"] != 0){
+        plus1Knop.disabled = false;
+      }
+      bedragString.innerText = virtueelBod["10"];
       break;
     case 13:
-      console.log("Verlaag 20");
+      virtueelBod["20"]--;
+      virtueelBedrag["20"]++;
+      if(virtueelBod["20"] === 0)
+      {
+        e.target.disabled = true;
+      }
+      if(virtueelBedrag["20"] != 0){
+        plus1Knop.disabled = false;
+      }
+      bedragString.innerText = virtueelBod["20"];
       break;
     case 19:
-      console.log("Verlaag 50");
+      virtueelBod["50"]--;
+      virtueelBedrag["50"]++;
+      if(virtueelBod["50"] === 0)
+      {
+        e.target.disabled = true;
+      }
+      if(virtueelBedrag["50"] != 0){
+        plus1Knop.disabled = false;
+      }
+      bedragString.innerText = virtueelBod["50"];
       break;
     case 25:
-      console.log("Verlaag 100");
+      virtueelBod["100"]--;
+      virtueelBedrag["100"]++;
+      if(virtueelBod["100"] === 0)
+      {
+        e.target.disabled = true;
+      }
+      if(virtueelBedrag["100"] != 0){
+        plus1Knop.disabled = false;
+      }
+      bedragString.innerText = virtueelBod["100"];
       break;
     case 31:
-      console.log("Verlaag 200");
+      virtueelBod["200"]--;
+      virtueelBedrag["200"]++;
+      if(virtueelBod["200"] === 0)
+      {
+        e.target.disabled = true;
+      }
+      if(virtueelBedrag["200"] != 0){
+        plus1Knop.disabled = false;
+      }
+      bedragString.innerText = virtueelBod["200"];
       break;
     case 37:
-      console.log("Verlaag 500");
+      virtueelBod["500"]--;
+      virtueelBedrag["500"]++;
+      if(virtueelBod["500"] === 0)
+      {
+        e.target.disabled = true;
+      }
+      if(virtueelBedrag["500"] != 0){
+        plus1Knop.disabled = false;
+      }
+      bedragString.innerText = virtueelBod["500"];
       break;
   }
 }
@@ -101,7 +244,6 @@ function verhoogBedrag(e){
   let min1Knop = e.target.parentNode.previousSibling.previousSibling.previousSibling.previousSibling.children[0];
   min1Knop.disabled = false;
 
-
   let child = e.target.parentNode;
   var i = 0;
   while( (child = child.previousSibling) != null ) {
@@ -109,27 +251,63 @@ function verhoogBedrag(e){
   }
   switch(i){
     case 5:
-      console.log("Verhoog 0");
+      virtueelBod["0"]++;
+      virtueelBedrag["0"]--;
+      if(virtueelBedrag["0"] === 0){
+        e.target.disabled = true;
+      }
+      bedragString.innerText = virtueelBod["0"];
       break;
     case 11:
-      console.log("Verhoog 10");
+      virtueelBod["10"]++;
+      virtueelBedrag["10"]--;
+      if(virtueelBedrag["10"] === 0){
+        e.target.disabled = true;
+      }
+      bedragString.innerText = virtueelBod["10"];
       break;
     case 17:
-      console.log("Verhoog 20");
+      virtueelBod["20"]++;
+      virtueelBedrag["20"]--;
+      if(virtueelBedrag["20"] === 0){
+        e.target.disabled = true;
+      }
+      bedragString.innerText = virtueelBod["20"];
       break;
     case 23:
-      console.log("Verhoog 50");
+      virtueelBod["50"]++;
+      virtueelBedrag["50"]--;
+      if(virtueelBedrag["50"] === 0){
+        e.target.disabled = true;
+      }
+      bedragString.innerText = virtueelBod["50"];
       break;
     case 29:
-      console.log("Verhoog 100");
+      virtueelBod["100"]++;
+      virtueelBedrag["100"]--;
+      if(virtueelBedrag["100"] === 0){
+        e.target.disabled = true;
+      }
+      bedragString.innerText = virtueelBod["100"];
       break;
     case 35:
-      console.log("Verhoog 200");
+      virtueelBod["200"]++;
+      virtueelBedrag["200"]--;
+      if(virtueelBedrag["200"] === 0){
+        e.target.disabled = true;
+      }
+      bedragString.innerText = virtueelBod["200"];
       break;
     case 41:
-      console.log("Verhoog 500");
+      virtueelBod["500"]++;
+      virtueelBedrag["500"]--;
+      if(virtueelBedrag["500"] === 0){
+        e.target.disabled = true;
+      }
+      bedragString.innerText = virtueelBod["500"];
       break;
   }
+  console.log(calculateTotal(virtueelBod));
 }
 
 // Log the messages from the server
